817580bd64fb5536c08d83ee873080fb8b1483ab
vincentjames-beep <vincent.james@hgs.hiddengeniusproject.org>
2026-07-15 14:45:52 -0400

    if- statement.py

---
+++ if- statement.py
@@ -0,0 +1,18 @@
+age = 18
+
+if age >= 18:
+    print("You are an adult.")
+  age = 16
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+  score = 85
+
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+else:
+    print("Grade: C or below")
