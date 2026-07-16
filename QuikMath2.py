982c974d5cec4e29b9087f32555c95d22c11de4e
vincentjames-beep <vincent.james@hgs.hiddengeniusproject.org>
2026-07-16 12:03:52 -0400

    QuikMath2.py

---
+++ QuikMath2.py
@@ -0,0 +1,35 @@
+<p> A Function is a reusable block of code that performs a specific task when it is called.<p>
+
+# Function to add two numbers
+def AddSum(num1, num2):
+    return num1 + num2
+
+# Function to subtract two numbers
+def Subtract(num1, num2):
+    return num1 - num2
+
+# Function to divide two numbers
+def Divide(num1, num2):
+    return num1 / num2
+
+# Function to multiply two numbers
+def Multiply(num1, num2):
+    return num1 * num2
+
+
+# Testing the functions
+print("Addition:", AddSum(10, 5))
+print("Subtraction:", Subtract(10, 5))
+print("Division:", Divide(10, 5))
+print("Multiplication:", Multiply(10, 5))
+
+def greet():
+    print("Hello My Names is Vincent!")
+  
+def add(a, b):
+    return a + b
+
+def greet(name):   # "name" is a parameter
+    print(name)
+
+greet(Kayda")      # "Kayda" is an argument
